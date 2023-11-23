bf56ae723960e425d78cd769fea796bc64aff4a1
Pavel Novik <dexyo1337@yandex.ru>
2023-11-23 22:40:12 +0300

    fix functions

--- scripts/index.js
+++ scripts/index.js
@@ -3,26 +3,28 @@ const template = document.querySelector('#card-template').content;
 const cardsContainer = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-function addCard(card, handleDeleteCard) {
+function createCard(card, handleDeleteCard) {
   const cardElement = template.querySelector('.card').cloneNode(true);
   const deleteButton = cardElement.querySelector('.card__delete-button');
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
 
-  deleteButton.addEventListener('click', (e) => handleDeleteCard(e.target));
+  deleteButton.addEventListener('click', handleDeleteCard);
 
-  cardElement.querySelector('.card__image').src = card.link;
-  cardElement.querySelector('.card__title').textContent = card.name;
-  cardElement.querySelector('.card__image').alt = card.name;
+  cardImage.src = card.link;
+  cardImage.alt = card.name;
+  cardTitle.textContent = card.name;
 
   return cardElement;
 }
 
 // @todo: Функция удаления карточки
-function handleDeleteCard(e) {
-  e.closest('.card').remove();
+function handleDeleteCard(event) {
+  event.target.closest('.card').remove();
 }
 
 // @todo: Вывести карточки на страницу
 initialCards.forEach((card) => {
-  const newCard = addCard(card, handleDeleteCard);
+  const newCard = createCard(card, handleDeleteCard);
   cardsContainer.append(newCard);
 });
